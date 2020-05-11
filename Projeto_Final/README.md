22c3fdb44f1ebbfa27b9a6a3e177f8d0b7426555
GustavoLopesBrito <61094662+GustavoLopesBrito@users.noreply.github.com>
2020-05-11 19:34:45 -0300

    Revert "Merge pull request #3 from iojoaomarcos/Projeto_Final"
    This reverts commit 0a74bcd2de8b5be643aec1b50603933a769f5d7e, reversing
    changes made to fc92f3beb69de546bd6bdd7bdf8043a9fb043d00.

--- Projeto_Final/README.md
+++
@@ -1,2 +0,0 @@
-# projeto_final_acoes
- Projeto_Final
--- Projeto_Final/android/app/src/main/kotlin/com/example/projeto_final_acoes/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.projeto_final_acoes
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
