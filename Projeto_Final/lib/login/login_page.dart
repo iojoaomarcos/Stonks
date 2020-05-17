22874a45408c7a7037c7f97482d9b94a3c0f87e3
FelipeGianni <felipe.gianni@hotmail.com>
2020-05-16 21:54:01 -0300

    Atualização Tela de Login
    Essa atualização é referente a tela de login onde foi implementada a autenticação com o Google e um novo estilo para a tela.

--- Projeto_Final/lib/login/login_page.dart
+++ Projeto_Final/lib/login/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_final_acoes/conversor_moedas/conversor_page.dart';
+import 'package:projeto_final_acoes/helpers/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -10,78 +11,75 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("Login"),
-        centerTitle: true,
-        backgroundColor: Colors.blueAccent,
-        actions: <Widget>[
-          FlatButton(
-            onPressed: (){},
-            child: Text(
-              "Criar Conta",
-              style: TextStyle(fontSize: 15.0)
-            ),
-            textColor: Colors.black,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.lightBlue[200], Colors.lightBlue[800]],
+            begin: FractionalOffset.topLeft,
+            end: FractionalOffset.bottomRight,
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp
           )
-        ],
-      ),
-      body: Form(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
+        ),
+        child: Center(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 250.0, bottom: 70.0),
+                child: Image(
+                  image: AssetImage("images/stonks.png"),
+                  height: 80.0
+                ),
+              ),
+              _signInButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _signInButton() {
+    return Container(
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40)
+        ),
+      ),
+      child: OutlineButton(
+        splashColor: Colors.grey,
+        onPressed: () {
+          authService.signInWithGoogle().whenComplete(() {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(
+                builder: (context) {
+                  return ConversorPage();
+                },
+              ),
+            );
+          });
+        },
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+        highlightElevation: 0,
+        borderSide: BorderSide(color: Colors.grey),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                style: TextStyle(color: Colors.blue, fontSize: 20.0),
-                decoration: InputDecoration(
-                  labelText: "E-mail",
-                  labelStyle: TextStyle(color: Colors.red)
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 30.0, bottom: 30.0),
-                child: TextFormField(
-                  keyboardType: TextInputType.text,
-                  style: TextStyle(color: Colors.blue, fontSize: 20.0),
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    labelStyle: TextStyle(color: Colors.red)
+              Image(image: AssetImage("images/google_logo.png"), height: 35.0),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  'Sign in with Google',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
                   ),
                 ),
-              ),
-              ButtonTheme(
-                height: 60.0,
-                child: RaisedButton(
-                  onPressed: (){
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => ConversorPage())
-                    );
-                  },
-                  shape: new RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(30.0)
-                  ),
-                  child: Text(
-                    "Enviar",
-                    style: TextStyle(color: Colors.white, fontSize: 30.0),
-                  ),
-                  color: Colors.red,
-                ),
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: FlatButton(
-                  onPressed: (){},
-                  child: Text("Esqueci minha senha",
-                    textAlign: TextAlign.right,
-                    style: TextStyle(color: Colors.blue, fontSize: 15.0),
-                  ),
-                  padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
-                ),
-              ),
+              )
             ],
           ),
         ),
