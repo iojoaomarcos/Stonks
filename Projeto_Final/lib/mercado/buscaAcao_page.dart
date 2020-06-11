e8e6e4816325c16f87112accee58dac64ed0702e
FelipeGianni <felipe.gianni@hotmail.com>
2020-06-11 13:43:57 -0300

    Tela 'Adicionar Posição'
    Desenvolvido tela para cadastrar uma posição em uma ação.
    
    Ao clicar no FloatingActionButton, abre a tela com a lista de ações do Ibovespa disponíveis na API. Ao clicar em uma delas, abre a tela para adicionar a posição de compra e, também, as informação do preço atual da ação e última variação.

--- Projeto_Final/lib/mercado/buscaAcao_page.dart
+++ Projeto_Final/lib/mercado/buscaAcao_page.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+
+import 'package:projeto_final_acoes/mercado/acaoBovespa.dart';
+import 'package:projeto_final_acoes/mercado/detalha_inclui_acao.dart';
 
 class BuscaAcaoPage extends StatefulWidget {
   @override
@@ -6,9 +10,38 @@ class BuscaAcaoPage extends StatefulWidget {
 }
 
 class _BuscaAcaoPageState extends State<BuscaAcaoPage> {
+  List<AcaoBovespa> bovespaList = []; //Lista das acoes do usuario
 
   Icon _cusIcon = Icon(Icons.search);
   Widget _cusSearchBar = Text("Busca Ação");
+
+  @override
+  void initState() {
+    super.initState();
+
+    DatabaseReference acaoBovespaRef = FirebaseDatabase.instance.reference().child("AcaoBovespa");
+
+    acaoBovespaRef.once().then((DataSnapshot snap) {
+      var key  = snap.value.keys;
+      var data = snap.value;
+
+      bovespaList.clear();
+
+      for (var individualKey in key) {
+        AcaoBovespa acaoBov = new AcaoBovespa(
+          data[individualKey]['name'],
+          data[individualKey]['symbol'],
+          individualKey,
+        );
+
+        bovespaList.add(acaoBov);
+      }
+
+      setState(() {
+        bovespaList.sort();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +77,46 @@ class _BuscaAcaoPageState extends State<BuscaAcaoPage> {
           )
         ],
         elevation: 20.0,
+      ),
+      body: ListView.builder(
+        itemCount: bovespaList.length,
+        itemBuilder: (context, index){
+          final name   = bovespaList[index].name;
+          final symbol = bovespaList[index].symbol;
+
+          return Column(
+            children: <Widget>[
+              ListTile(
+                title: Text('$name',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    )),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text(
+                    '$symbol',
+                    style: TextStyle(
+                        color: Colors.grey[400], fontSize: 14.0),
+                  ),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetalhaIncluiAcao(symbol: symbol, name: name)
+                    ),
+                );}, //Muda para página contendo detalhes da ação passando como parametro o simbolo
+              ),
+              Divider(
+                height: 2.0,
+                color: Colors.grey,
+              )
+            ],
+          );
+        }
       ),
     );
   }
