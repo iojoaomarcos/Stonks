3472831d3cd3793c08dec5532d73419978120857
João Marcos <joao.marcossantos@outlook.com>
2020-06-14 17:46:40 -0300

    ordenando as ações em ordem alfabetica

--- Projeto_Final/lib/mercado/buscaAcao_page.dart
+++ Projeto_Final/lib/mercado/buscaAcao_page.dart
@@ -19,10 +19,11 @@ class _BuscaAcaoPageState extends State<BuscaAcaoPage> {
   void initState() {
     super.initState();
 
-    DatabaseReference acaoBovespaRef = FirebaseDatabase.instance.reference().child("AcaoBovespa");
+    DatabaseReference acaoBovespaRef =
+        FirebaseDatabase.instance.reference().child("AcaoBovespa");
 
     acaoBovespaRef.once().then((DataSnapshot snap) {
-      var key  = snap.value.keys;
+      var key = snap.value.keys;
       var data = snap.value;
 
       bovespaList.clear();
@@ -38,7 +39,9 @@ class _BuscaAcaoPageState extends State<BuscaAcaoPage> {
       }
 
       setState(() {
-        bovespaList.sort();
+        bovespaList.sort((a, b) => a.name.compareTo(b.name));
+        print('Quantidade de ações disponiveis: ' +
+            bovespaList.length.toString());
       });
     });
   }
@@ -52,72 +55,71 @@ class _BuscaAcaoPageState extends State<BuscaAcaoPage> {
         backgroundColor: Colors.blueAccent,
         actions: <Widget>[
           IconButton(
-            icon: _cusIcon,
-            onPressed: (){
-              setState(() {
-                if(this._cusIcon.icon == Icons.search){
-                  this._cusIcon = Icon(Icons.cancel);
-                  this._cusSearchBar = TextField(
-                    textInputAction: TextInputAction.go,
-                    autofocus: true,
-                    decoration: InputDecoration(
-                      enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
-                      focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
-                      hintText: "Procurar...",
-                    ),
-                    style: TextStyle(color: Colors.white, fontSize: 16.0),
-                  );
-                }
-                else{
-                  this._cusIcon = Icon(Icons.search);
-                  this._cusSearchBar = Text("Busca Ação");
-                }
-              });
-            }
-          )
+              icon: _cusIcon,
+              onPressed: () {
+                setState(() {
+                  if (this._cusIcon.icon == Icons.search) {
+                    this._cusIcon = Icon(Icons.cancel);
+                    this._cusSearchBar = TextField(
+                      textInputAction: TextInputAction.go,
+                      autofocus: true,
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black)),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black)),
+                        hintText: "Procurar...",
+                      ),
+                      style: TextStyle(color: Colors.white, fontSize: 16.0),
+                    );
+                  } else {
+                    this._cusIcon = Icon(Icons.search);
+                    this._cusSearchBar = Text("Busca Ação");
+                  }
+                });
+              })
         ],
         elevation: 20.0,
       ),
       body: ListView.builder(
-        itemCount: bovespaList.length,
-        itemBuilder: (context, index){
-          final name   = bovespaList[index].name;
-          final symbol = bovespaList[index].symbol;
+          itemCount: bovespaList.length,
+          itemBuilder: (context, index) {
+            final name = bovespaList[index].name;
+            final symbol = bovespaList[index].symbol;
 
-          return Column(
-            children: <Widget>[
-              ListTile(
-                title: Text('$name',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    )),
-                subtitle: Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: Text(
-                    '$symbol',
-                    style: TextStyle(
-                        color: Colors.grey[400], fontSize: 14.0),
-                  ),
-                ),
-                trailing: Icon(Icons.arrow_forward_ios),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DetalhaIncluiAcao(symbol: symbol, name: name)
+            return Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text('$name',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      '$symbol',
+                      style: TextStyle(color: Colors.grey[400], fontSize: 14.0),
                     ),
-                );}, //Muda para página contendo detalhes da ação passando como parametro o simbolo
-              ),
-              Divider(
-                height: 2.0,
-                color: Colors.grey,
-              )
-            ],
-          );
-        }
-      ),
+                  ),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              DetalhaIncluiAcao(symbol: symbol, name: name)),
+                    );
+                  }, //Muda para página contendo detalhes da ação passando como parametro o simbolo
+                ),
+                Divider(
+                  height: 2.0,
+                  color: Colors.grey,
+                )
+              ],
+            );
+          }),
     );
   }
 }
