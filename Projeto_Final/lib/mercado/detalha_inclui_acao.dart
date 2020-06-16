a8340eb242a2314aa097d23cc9667d385b13b81a
Guilherne Camargo <60949331+guihcamargo@users.noreply.github.com>
2020-06-16 16:56:31 -0300

    traducao tela 1
    detalhe inclui acao

--- Projeto_Final/lib/mercado/detalha_inclui_acao.dart
+++ Projeto_Final/lib/mercado/detalha_inclui_acao.dart
@@ -186,7 +186,7 @@ class _DetalhaIncluiAcaoState extends State<DetalhaIncluiAcao> {
               child: RaisedButton(
                 color: Colors.blueAccent,
                 onPressed: () {}, //grava posição no banco
-                child: Text('Adicionar Compra',
+                child: Text('Add Purchase',
                     style: TextStyle(
                         color: Colors.white, fontSize: setWidth(20.0))),
               ),
